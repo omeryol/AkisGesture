7ef4ebe499e771515cbe68f134eba82db5ba924b
omer <40967962+omeryol@users.noreply.github.com>
2026-08-01 04:00:30 +0300

    ui: refresh gesture feedback visuals

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.omeryol.akisgesture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.16"
+        versionCode = 5
+        versionName = "1.1.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
