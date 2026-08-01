b7f8040b9d848148cded54f72c6e6f626c8d21ba
omer <40967962+omeryol@users.noreply.github.com>
2026-08-01 05:58:59 +0300

    docs: version ocean wave restoration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         targetSdk = 35
         versionCode = 33
         versionName = "1.1.45"
-        versionCode = 38
-        versionName = "1.1.50"
+        versionCode = 39
+        versionName = "1.1.51"
     }
 
     buildTypes {
