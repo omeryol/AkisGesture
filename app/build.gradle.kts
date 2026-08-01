eb7a942c6115d2c18d732c0e4f22e442175dddb8
omer <40967962+omeryol@users.noreply.github.com>
2026-08-01 04:01:31 +0300

    ui: strengthen gesture icon weight

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.omeryol.akisgesture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.17"
+        versionCode = 6
+        versionName = "1.1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
