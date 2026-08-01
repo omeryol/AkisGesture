8c3ee0797e2422d1419bb0fa9898afcef82f2723
omer <40967962+omeryol@users.noreply.github.com>
2026-08-01 05:57:13 +0300

    feat: upgrade existing animations with transition materials

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         targetSdk = 35
         versionCode = 33
         versionName = "1.1.45"
-        versionCode = 37
-        versionName = "1.1.49"
+        versionCode = 38
+        versionName = "1.1.50"
     }
 
     buildTypes {
