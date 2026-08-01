71d1b6ad4fdac4efc602eb0644b946888818eba3
omer <40967962+omeryol@users.noreply.github.com>
2026-08-01 05:14:16 +0300

    feat: add black hole pull animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.omeryol.akisgesture"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.1.41"
+        versionCode = 30
+        versionName = "1.1.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
